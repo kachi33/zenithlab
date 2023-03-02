be96838073e16d4040757e3ce875261ceedcbd08
kachi33 <ezeahjessica@gmail.com>
2023-03-02 16:21:48 +0100

    Nav component

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
 'use strict'
+import { Route } from "react-router-dom";
 
 const App = () => {
-  return React.createElement('p', null, 'Hello World');
+  return (
+    React.createElement('nav', {className: 'nav-bar'}, 
+      React.createElement('a', {href:'/'}, 'Home'),
+      React.createElement('a', {href:'/About'}, 'Who we are'),
+      React.createElement('a', {href:'/Services'}, `What we've done`),
+      React.createElement('a', {href:'/Contact-us'}, 'Contact us')
+    )
+  );
 };
 
 ReactDOM.render(
