138c817a56a5eff0a72b8863d2b7ca8ff2216a5b
kachi33 <ezeahjessica@gmail.com>
2023-03-03 13:06:46 +0100

    Navbar not properly styled

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 'use strict'
-import Navbar from "./Navbar";
+
+const Navbar = () =>{
+  return(
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a className="navbar-brand">
+              <img src='/imgs/favicon.ico' width='30' height='30'/>Zenith Medical laboratory
+          </a>
+          {/* <button></button> */}
+          <a href='/' className='text-white nav-link'>Home</a>
+          <a href='/About' className='text-white nav-link'>Who we are</a>
+          <a href='/Services' className='text-white nav-link'>What we've done</a>
+          <a href='/Contact-us' className='text-white nav-link'>Contact us</a>
+  
+      </nav>
+     </div>
+   );
+ }
 
 const App = () => {
   return (
-      <Navbar/>
+    <Navbar/>
   );
 };
 
-const render = ReactDOM.render(<App/>, document.getElementById('root')
-);
-
-babel.transform("const render = " + render.toString()).code;
-render();
+ReactDOM.render(<App/>, document.getElementById('root'));
--- Navbar.js
+++ Navbar.js
@@ -1,17 +1,18 @@
-`use strict`
+// `use strict`
 
-const Navbar = () =>{
- return(
-    <nav className="navbar">
-        <a className="navbar-brand">
-            <img src='/docs/4.0/assets/brand/bootstrap-solid.svg' width='30' height='30'>Zenith Medical laboratory</img>
-        </a>
-        <a href='/'>Home</a>
-        <a href='/About'>Who we are</a>
-        <a href='/Services'>What we've done</a>
-        <a href='/Contact-us'>Contact us</a>
-
-    </nav>
-  );
-}
- export default Navbar;
+// const Navbar = () =>{
+//     return(
+//        <nav className="navbar">
+//            <a className="navbar-brand">
+//                <img src='/docs/4.0/assets/brand/bootstrap-solid.svg' width='30' height='30'/>Zenith Medical laboratory
+//            </a>
+//            <a href='/'>Home</a>
+//            <a href='/About'>Who we are</a>
+//            <a href='/Services'>What we've done</a>
+//            <a href='/Contact-us'>Contact us</a>
+   
+//        </nav>
+//      );
+//    }
+  
+//  export default Navbar;
