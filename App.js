de69edbe9cfdcd5cb557bbe2c3c2d18bdfe65581
kachi33 <ezeahjessica@gmail.com>
2023-03-05 15:03:39 +0100

    Remove Bootstrap styles frm Navbar component

--- App.js
+++ App.js
@@ -2,24 +2,22 @@
 
 const Navbar = () =>{
   return(
-    <div className="container">
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <a className="navbar-brand">
-              <img src='/imgs/favicon.ico' width='30' height='30'/>Zenith Medical laboratory
+      <nav className="navbar">
+          <a>
+              <img src='/imgs/favicon.ico'/>Zenith Medical laboratory
           </a>
-          {/* <button></button> */}
-          <a href='/' className='text-white nav-link'>Home</a>
-          <a href='/About' className='text-white nav-link'>Who we are</a>
-          <a href='/Services' className='text-white nav-link'>What we've done</a>
-          <a href='/Contact-us' className='text-white nav-link'>Contact us</a>
+          <a href='/'>Home</a>
+          <a href='/About'>Who we are</a>
+          <a href='/Services' >What we've done</a>
+          <a href='/Contact-us' >Contact us</a>
   
       </nav>
-     </div>
    );
  }
 
 const App = () => {
   return (
+    
     <Navbar/>
   );
 };
