e12043635d1ba206f4c4e5fa4de8d276de77fa04
kachi33 <ezeahjessica@gmail.com>
2023-03-05 15:56:16 +0100

    feat(Navbar):Refactor Navbar for reusablity
    - Remove Bootstrap from Navbar component
    - The Navbar component was removed from the App component and placed into its own file for improved reusability.

--- App.js
+++ App.js
@@ -1,19 +1,5 @@
 'use strict'
-
-const Navbar = () =>{
-  return(
-      <nav className="navbar">
-          <a>
-              <img src='/imgs/favicon.ico'/>Zenith Medical laboratory
-          </a>
-          <a href='/'>Home</a>
-          <a href='/About'>Who we are</a>
-          <a href='/Services' >What we've done</a>
-          <a href='/Contact-us' >Contact us</a>
-  
-      </nav>
-   );
- }
+import Navbar from "./Navbar";
 
 const App = () => {
   return (
--- Navbar.js
+++ Navbar.js
@@ -1,18 +1,18 @@
-// `use strict`
+`use strict`
 
-// const Navbar = () =>{
-//     return(
-//        <nav className="navbar">
-//            <a className="navbar-brand">
-//                <img src='/docs/4.0/assets/brand/bootstrap-solid.svg' width='30' height='30'/>Zenith Medical laboratory
-//            </a>
-//            <a href='/'>Home</a>
-//            <a href='/About'>Who we are</a>
-//            <a href='/Services'>What we've done</a>
-//            <a href='/Contact-us'>Contact us</a>
-   
-//        </nav>
-//      );
-//    }
+const Navbar = () =>{
+    return(
+        <nav className="navbar">
+            <a>
+                <img src='/imgs/favicon.ico'/>Zenith Medical laboratory
+            </a>
+            <a href='/'>Home</a>
+            <a href='/About'>Who we are</a>
+            <a href='/Services' >What we've done</a>
+            <a href='/Contact-us' >Contact us</a>
+    
+        </nav>
+     );
+   }
   
-//  export default Navbar;
+ export default Navbar;
