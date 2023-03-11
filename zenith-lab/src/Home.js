2f1522e4ded89d446aeb0afb831fca37ba10c3e7
kachi33 <ezeahjessica@gmail.com>
2023-03-11 22:48:02 +0100

    chore(home): Add style to home component

--- zenith-lab/src/Home.js
+++ zenith-lab/src/Home.js
@@ -1,7 +1,14 @@
 const Home = () => {
     return ( 
         <div className="home">
-            <h1> Welcome</h1>
+            <div className="banner">
+                <h1> Welcome</h1>
+            </div>
+            <div>
+                <section>
+
+                </section>
+            </div>
         </div>
      );
 }
